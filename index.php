edd6fedd31a40bdaa4dd75b67856f4908de88d0b
Jacob <jtgraham38@gmail.com>
2023-08-28 21:17:08 -0400

    Fix flash messages.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //start session
 session_start();
 
-//clean ob
+//clean output buffer
 ob_clean();
 
 //include helper files
@@ -21,22 +21,23 @@ if (isset($routes[$requested_path])) {
     $func_name = $routes[$requested_path];
     if (function_exists($func_name)) {
 
+        //generate page content/handle forms
         call_user_func($func_name);
+
         //clean csrf tokens
         clean_csrf_tokens();
         
         //handle flash messages
-        //TODO: fix flash messages
-        echo $_SESSION['flash_message'];
-        if ($_SESSION['flash_message']) {
-            ?> 
-                <div style="position: fixed; left: 10px; top: 5px; background-color: lightgray; z-index: 1000;" class="notification">
-                    <button class="delete" onclick="event.target.parentNode.remove()"></button>
-                    <?= $_SESSION['flash_message'] ?>
-                </div>
-            <?php
-            unset($_SESSION['flash_message']); // clear the flash message session variable
-            //^ TODO: this line causes flash messages not to work
+        if (!strpos($requested_path, 'handle')){    //if it is not a handler function... (all handlers must contain "handle" in the path)
+            if (isset($_SESSION['flash_message']) && $_SESSION['flash_message']) {
+                ?> 
+                    <div style="position: fixed; left: 10px; top: 5px; background-color: lightgray; z-index: 1000;" class="notification">
+                        <button class="delete" onclick="event.target.parentNode.remove()"></button>
+                        <?= htmlspecialchars($_SESSION['flash_message']) ?>
+                    </div>
+                <?php
+                unset($_SESSION['flash_message']); // clear the flash message session variable
+            }
         }
     } else {
         echo "404 - Page not found (no func)";
